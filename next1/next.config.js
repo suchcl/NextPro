9cfa3b5cd6ddb4754e79040bf562bf680c546143
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2021-12-14 21:18:00 +0800

    next应用导入样式

--- next1/next.config.js
+++ next1/next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    dev: "DEVELOPMENT"
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/about/about",
+  //       destination: "/about"
+  //     }
+  //   ]
+  // },
+  // basePath: "/NextPro"
 }
--- next1/pages/user/profile.jsx
+++ next1/pages/user/profile.jsx
@@ -1,11 +1,23 @@
 import Nav from "../components/Nav";
 import ProfileStyle from "../../styles/Profile.module.css";
 
+const user = {
+  fontSize: 26,
+  color: "#369",
+  fontWeight: "normal"
+};
+
 function Profile() {
   return (
     <>
     <Nav />
       <p className={ProfileStyle.text}>个人中心</p>
+      <p>当前环境是:{process.env.dev}</p>
+      <p>当前的nodejs环境变量：{process.env.NODE_ENV}</p>
+      <div className="user">
+        <h2 style={user}>姓名: Nicholas Zakas</h2>
+        <p style={{color: "#090", fontSize: 18}}>年龄：18</p>
+      </div>
     </>
   );
 }
